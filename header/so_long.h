3164b618bf5df8abc8c8412f9dc00e25461af015
lumikil <kilpelainenlumi@gmail.com>
2024-01-11 13:44:30 +0200

    update headers & add game_logic

---
+++ header/so_long.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 13:39:15 by lkilpela          #+#    #+#             */
+/*   Updated: 2024/01/11 13:41:55 by lkilpela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include "./MLX42/include/MLX42/MLX42.h"
