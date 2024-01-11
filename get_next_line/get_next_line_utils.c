c1e83a03a1328684c29a115ac4f6f9a0f90bf70f
lumikil <kilpelainenlumi@gmail.com>
2024-01-11 13:38:05 +0200

    add gnl

---
+++ get_next_line/get_next_line_utils.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/24 22:23:26 by lkilpela          #+#    #+#             */
+/*   Updated: 2023/12/18 15:39:11 by lkilpela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(char *s, int c)
+{
+	if (s == NULL)
+		return (NULL);
+	while (*s != '\0')
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		++s;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (0);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+		return (0);
+	while (s[i] != 0)
+		i++;
+	return (i);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+	size_t				i;
+
+	if (dst == src || n == 0)
+		return (dst);
+	d = (unsigned char *)dst;
+	s = (const unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dst);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*s2;
+	int		i;
+
+	if (s1 == NULL)
+		return (NULL);
+	i = ft_strlen(s1);
+	s2 = (char *)malloc(sizeof(char) * (i + 1));
+	if (s2 == NULL)
+		return (NULL);
+	ft_memcpy(s2, s1, i + 1);
+	return (s2);
+}
