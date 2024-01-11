b509d6fe6389099056d44643e2dc1628cdae1ef7
Lumi Kilpeläinen <lumivo-taivainen@Lumis-Air.home>
2024-01-11 23:31:06 +0200

    rename folder

---
+++ srcs/so_long.h
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/11 13:39:15 by lkilpela          #+#    #+#             */
+/*   Updated: 2024/01/11 22:35:55 by lkilpela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include "./MLX42/include/MLX42/MLX42.h"
+
+# define OPEN_SPACE '0'
+# define WALL '1'
+# define EXIT 'E'
+# define COLLECTIBLE 'C'
+# define PLAYER 'P'
+
+typedef struct	s_game 
+{
+	int		fox_x;
+	int		fox_y;
+	int		*apple_x;
+	int		*apple_y;
+	int		apple_count;
+	int		*tree_x;
+	int		*tree_y;
+	int		tree_count;
+	int		door_x;
+	int		door_y;
+	char	**map;
+	int		map_width;
+	int		map_height;
+}				t_game;
