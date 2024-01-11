b4d1a7a636c7c43ddc7a0c714336b80e3ee5236a
lumikil <kilpelainenlumi@gmail.com>
2024-01-11 10:36:08 +0200

    add main

---
+++ main.c
@@ -0,0 +1,16 @@
+/**
+ * Main MLX handle, carries important data in regards to the program.
+ * @param window The window itself.
+ * @param context Abstracted opengl data.
+ * @param width The width of the window.
+ * @param height The height of the window.
+ * @param delta_time The time difference between the previous frame and the current frame.
+ */
+typedef struct mlx
+{
+	void*		window;
+	void*		context;
+	int32_t		width;
+	int32_t		height;
+	double		delta_time;
+}	mlx_t;
