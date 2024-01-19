1c06a9af3badc0eea75d317e1a8772163866f685
lumikil <kilpelainenlumi@gmail.com>
2024-01-19 10:54:50 +0200

    update allocate_map

--- srcs/map/get_map.c
+++ srcs/map/get_map.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 23:33:45 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/01/19 10:01:30 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/01/19 10:53:55 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,9 @@ static int	get_map_width(char *map_file)
 //Calculate the number of lines in map file
 static int	get_map_height(char *map_file)
 {
-	int i;
-	int fd;
-	int	height;
+	int 	i;
+	int 	fd;
+	char	chr;
 	
 	i = 0;
 	fd = open(map_file, O_RDONLY);
@@ -39,31 +39,22 @@ static int	get_map_height(char *map_file)
 		perror("Error opening file\n");
 		return (-1);
 	}
-	height = get_next_line(fd, &map_file);
-	while (height > 0)
-		i++;
-	if (height < 0)
+	while (read(fd, &chr, 1) > 0)
 	{
-		perror("Error reading file\n");
-		close(fd);
-		return (-1);
+		if (chr == '\n')
+			i++;
 	}
 	close(fd);
 	return (i);
 }
 
-int	load_map(t_map *map, char *map_file)
+int	allocate_map(t_map *map, char *map_file)
 {
 	map->row = get_map_height(map_file);
-	if(map->row < 0)
-	{
-		perror("Error");
-		return (-1);
-	}
 	map->column = get_map_width(map_file)
-	if(map->column < 0)
+	if(map->column < 0 || map->row < 0)
 	{
-		perror("Error");
+		perror("Error getting map dimensions\n");
 		return (-1);
 	}
 	alloc_map_grid(map);
@@ -72,45 +63,28 @@ int	load_map(t_map *map, char *map_file)
 void	alloc_map_grid(t_map *map)
 {
 	int	i;
+	int j;
 	
 	map->grid = malloc(sizeof(char *) * map->row);
 	if (map->grid == NULL)
-		exit(-1);
-	i = 0;
-	while(i < map->row)
 	{
-		map->grid[i] = malloc(sizeof(char) * map->column);
-		if (map->grid == NULL)
-			exit(-1);
-		i++;
-	}
-}
-
-int	load_map_grid(t_map *map, char *map_file)
-{
-	int		fd;
-	int		i;
-	int		j;
-	char	*line;
-	
-	fd = open(map_file, O_RDONLY);
-	if (fd < 0)
-	{
-		perror("Error opening file\n");
+		perror("Error allocating memory for map grid\n");
+		free(map);
 		return (-1);
 	}
 	i = 0;
-	while (i < map->row)
+	while(i++ < map->row)
 	{
-		j = 0;
-		get_next_line(fd, &line);
-		while (j < map->column)
+		map->grid[i] = malloc(sizeof(char) * (map->column + 1));
+		if (map->grid[i] == NULL)
 		{
-			map->grid[i][j] = line[j];
-			j++;
+			perror("Error allocating memory for map grid\n");
+			j = 0;
+			while (j < i)
+				free(map->grid[j++]);
+			free(map->grid);
+			free(map);
+			return (-1);
 		}
-		i++;
 	}
-	close(fd);
-	return (0);	
 }
