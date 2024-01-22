06c24f9946fd72288ff2839d6443a8613ed6ebb6
lumikil <kilpelainenlumi@gmail.com>
2024-01-22 08:48:54 +0200

    update

--- srcs/player.c
+++ srcs/player.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 17:25:02 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/01/22 08:35:46 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/01/22 08:48:46 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ void	key_hook(mlx_key_data_t keydata, void* param)
 {
 	t_game* game = param;
 	int dx = 0, dy = 0;
-	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS) {
+	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS) 
+	{
 		mlx_close_window(game->mlx_ptr);
 		return;
 	}
@@ -28,23 +29,22 @@ void	key_hook(mlx_key_data_t keydata, void* param)
 		dx = -1;
 	if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_PRESS)
 		dx = 1;
-		2
-		03,120
-		900
-		                  p0´11´´´´´´´´´´´´´´´´,3 24022110	if(dx != 0 || dy != 0) {
-		if(game->map.grid[game->player.y+dy][game->player.x+dx] != '1') {
+	if(dx != 0 || dy != 0) 
+	{
+		if(game->map.grid[game->player.y+dy][game->player.x+dx] != '1') 
+		{
 			game->tiles.player->instances[0].y += dy*TILE_SIZE;
 			game->tiles.player->instances[0].x += dx*TILE_SIZE;
 			game->player.y += dy;
 			game->player.x += dx;
 			game->move_count++;
-			if(game->map.grid[game->player.y][game->player.x] == 'C') {
+			if(game->map.grid[game->player.y][game->player.x] == 'C') 
+			{
 				game->collectibles_count++;
 				printf("Collectibles found: %d\n", game->collectibles_count);
 			}	
-			if(game->map.grid[game->player.y][game->player.x] == 'E') {				
+			if(game->map.grid[game->player.y][game->player.x] == 'E') 			
 				printf("Congratulations, you found exit\n");
-			}
 			printf("step %d\n", game->move_count);
 		}
 	}
@@ -53,4 +53,4 @@ void	key_hook(mlx_key_data_t keydata, void* param)
 void init_player_movement(t_game *game)
 {
     mlx_key_hook(game->mlx_ptr, key_hook, game);
-}7-+'åä
+}
