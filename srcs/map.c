d4b2b42072727b021883979c0cf2357cac0d80b9
lumikil <kilpelainenlumi@gmail.com>
2024-01-24 09:09:45 +0200

    add validate_map.c

--- srcs/map.c
+++ srcs/map.c
@@ -6,33 +6,35 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 13:27:25 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/01/23 21:46:18 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/01/24 08:51:10 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static int get_line_width(char *line)
+static int	get_line_width(char *line)
 {
 	int	len;
 
 	len = ft_strlen(line);
 	if (len > 0 && line[len - 1] == '\n')
 		len--;
-	return (len);		
+	return (len);
 }
-static int add_to_map(t_game *game, char *line)
+
+static int	add_to_map(t_game *game, char *line)
 {
-    int i;
-    
+	int		i;
+	char	**new_arr;
+
+	i = 0;
 	if (game->map.height == 0)
 		game->map.width = get_line_width(line);
 	else if (game->map.width != get_line_width(line))
-		return(ERROR_INVALID_MAP);
-    char **new_arr = malloc(sizeof(char *) * game->map.height + 1);
-    if (!new_arr)
-        return(ERROR_ALLOCATING_MEMORY);
-    i = 0;
+		return (ERROR_INVALID_MAP);
+	new_arr = malloc(sizeof(char *) * game->map.height + 1);
+	if (!new_arr)
+		return (ERROR_ALLOCATING_MEMORY);
 	while (i < game->map.height)
 	{
 		new_arr[i] = game->map.grid[i];
@@ -44,7 +46,8 @@ static int add_to_map(t_game *game, char *line)
 	return (0);
 }
 
-int load_map(t_game *game, char *map_file)
+// status error : invalid map or memory allocation
+int	load_map(t_game *game, char *map_file)
 {
 	int		fd;
 	char	*line;
@@ -53,17 +56,17 @@ int load_map(t_game *game, char *map_file)
 	fd = open(map_file, O_RDONLY);
 	if (fd < 0)
 		return (ERROR_OPENING_FILE);
-    line = get_next_line(fd);
+	line = get_next_line(fd);
 	while (line != NULL)
 	{
 		status = add_to_map(game, line);
 		if (status < 0)
-		{	
+		{
 			free(line);
 			close(fd);
-			return (status);// error : invalid map or memory allocation
+			return (status);
 		}
-		line = get_next_line(fd);		
+		line = get_next_line(fd);
 	}
 	close(fd);
 	return (0);
