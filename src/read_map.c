b91f7b3e7db2c92725f665016359f98ef8061b74
lumikil <kilpelainenlumi@gmail.com>
2024-02-09 13:42:20 +0200

    Fix issue with empty lines in load_map function

--- src/read_map.c
+++ src/read_map.c
@@ -6,7 +6,7 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 13:27:25 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/09 13:35:14 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/02/09 13:42:19 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,12 @@ int	load_map(t_game *game, char *map_file)
 	line = get_next_line(fd);
 	while (line != NULL)
 	{
+		if (line[0] == '\n')
+		{
+			free(line);
+			close(fd);
+			return (ERROR_EMPTY_LINE);
+		}
 		status = add_to_map(game, line);
 		if (status != 0)
 		{
@@ -68,12 +74,7 @@ int	load_map(t_game *game, char *map_file)
 		}
 		line = get_next_line(fd);
 	}
-	if (line[0] == '\n')
-		{
-			free(line);
-			close(fd);
-			return (ERROR_EMPTY_LINE);
-		}
+
 	close(fd);
 	return (validate_map(game));
 }
