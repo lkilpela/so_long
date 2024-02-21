c4ddd0b97b794f74e4fbdc834f1c5c71b59c6786
lumikil <kilpelainenlumi@gmail.com>
2024-02-21 09:17:49 +0200

    Refactor step function to use t_position struct

--- src/validate_path.c
+++ src/validate_path.c
@@ -6,28 +6,36 @@
 /*   By: lkilpela <lkilpela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 11:24:50 by lkilpela          #+#    #+#             */
-/*   Updated: 2024/02/21 09:01:06 by lkilpela         ###   ########.fr       */
+/*   Updated: 2024/02/21 09:17:28 by lkilpela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-static int	step(t_game *game, int x, int y, char element)
+t_position make_pos(int x, int y)
+{
+	t_position pos;
+
+	pos.x = x;
+	pos.y = y;
+	return (pos);
+}
+
+static int	step(t_game *game, char	*new_arr, t_position pos, char element)
 {
 	int		i;
-	char	*new_arr;
 
-	i = y * game->map.width + x;
-	if (game->map.grid[y][x] == element)
+	i = pos.y * game->map.width + pos.x;
+	if (game->map.grid[pos.y][pos.x] == element)
 		return (1);
-	if (new_arr[i] || game->map.grid[y][x] == WALL
-		|| game->map.grid[y][x] == EXIT)
+	if (new_arr[i] || game->map.grid[pos.y][pos.x] == WALL
+		|| game->map.grid[pos.y][pos.x] == EXIT)
 		return (0);
 	new_arr[i] = 1;
-	return (step(game, new_arr, x - 1, y, element)
-		|| step(game, new_arr, x + 1, y, element)
-		|| step(game, new_arr, x, y - 1, element)
-		|| step(game, new_arr, x, y + 1, element));
+	return (step(game, new_arr, make_pos(x - 1, y), element)
+		|| step(game, new_arr, make_pos(x + 1, y), element)
+		|| step(game, new_arr, make_pos(x, y - 1), element)
+		|| step(game, new_arr, make_pos(x, y + 1), element));
 }
 
 int	validate_path(t_game *game, char element)
